81664604ed7537e02cca2db54201e00a76269ceb
Sumit Chahar <chahar.sumit888@gmail.com>
2022-04-06 23:04:01 +0530

    Add cors policy for fetch api requests

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
   next();
 });
 
